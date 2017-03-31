6ebc06dcc8cfdacd83521df222926c6542d18296
amagaldi <adolfo.magaldi@im.unam.mx>
2017-03-30 18:41:28 -0600

    nuevos perfil vertical file

--- perfil_vertical_vel_x_y_z.py
+++ perfil_vertical_vel_x_y_z.py
@@ -46,6 +46,9 @@ counter1=collections.Counter(R)
 re=(counter1.keys())
 re=sorted(re)
 ak=[]
+#aqui lo que graficamos es CI, si hacemos un loop para que grafique sobre 
+#XW vel x , YW vel en y y  ZW vel en z ya esta o tal vez repetir estas lineas de abajo
+# 3 veces más
 for kk in range(len(re)):
     aj=[]
     for k in range(len(CI)):
@@ -59,7 +62,68 @@ for kk in range(len(re)):
 
 plt.plot(ak,re)
 
-#plt.xlabel('Velocidad Z [m/s]') 
+plt.xlabel('CNR') 
 plt.xlabel('CI') 
 plt.ylabel('altura (m.a.g.l.)')
+plt.title('2017-02-25')      
+
+plt.show()
+ak=[]
+for kk in range(len(re)):
+    ax=[]
+    for k in range(len(XW)):
+        if R[k]==re[kk]:
+            ax.append(XW[k])
+            abx=np.nanmean(ax)
+            
+    ak.append(abx)  
+
+
+
+plt.plot(ak,re)
+
+plt.xlabel('Velocidad X') 
+plt.xlabel('Velocidad [m/s]') 
+plt.ylabel('altura (m.a.g.l.)')
+plt.title('2017-02-25')  
+
+plt.show()
+ak=[]
+for kk in range(len(re)):
+    ay=[]
+    for k in range(len(YW)):
+        if R[k]==re[kk]:
+            ay.append(YW[k])
+            aby=np.nanmean(ay)
+            
+    ak.append(aby)  
+
+
+
+plt.plot(ak,re)
+
+plt.xlabel('Velocidad Y') 
+plt.xlabel('Velocidad [m/s]') 
+plt.ylabel('altura (m.a.g.l.)')
 plt.title('2017-02-25')         
+
+plt.show()
+ak=[]
+for kk in range(len(re)):
+    az=[]
+    for k in range(len(ZW)):
+        if R[k]==re[kk]:
+            az.append(ZW[k])
+            abz=np.nanmean(az)
+            
+    ak.append(abz)  
+
+
+
+plt.plot(ak,re)
+
+plt.xlabel('Velocidad Z') 
+plt.xlabel('Velocidad [m/s]') 
+plt.ylabel('altura (m.a.g.l.)')
+plt.title('2017-02-25')  
+plt.show()
